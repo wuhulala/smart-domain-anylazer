42196f72fe8d1f89e450c7171ca2e7bc58940811
wuhulala <xueaohui_com@163.com>
2020-02-10 22:25:17 +0800

    init project

---
+++ backend-services/server/src/main/java/org/wuhulala/sda/SdaApplication.java
@@ -0,0 +1,13 @@
+package org.wuhulala.sda;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaApplication.class, args);
+    }
+}
